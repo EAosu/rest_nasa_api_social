52ac53672297338e50b6221d72dbf763e0d626bf
nadav <nadavma@edu.hac.ac.il>
2022-12-27 01:05:40 +0200

    lil-stylish in the house

--- public/javascripts/funcs.js
+++ public/javascripts/funcs.js
@@ -1,7 +1,7 @@
-let currDate = new Date()
+let endDate = new Date()
 let batchSize = 3;
-
-
+let displayIndex =0
+const defaultStartDate = new Date("1995-06-16")
 const validateName = (event) =>{
     event.preventDefault()
     name = document.getElementById("name").value.trim()
@@ -17,20 +17,22 @@ const validateName = (event) =>{
 
 function displayImagesFromURL(event) {
     event.preventDefault()
-    let endDate = new Date(document.getElementById("endDate").value)
+
+    endDate = new Date(document.getElementById("endDate").value)
     // Fetch the JSON from the given URL
-    fetch(getUrl(currDate)).then(response => response.json()).then(function(data){
+    fetch(getUrl(endDate)).then(response => response.json()).then(function(data){
         //currentIndex = 0 //since its global and we made a new request
         document.getElementById("imagesList").innerHTML = "";
         displayImagesBatch(data)
         //I didnt use document in purpose
         window.addEventListener("scroll", function(event){
             event.preventDefault()
+
             const scrollY = window.scrollY + window.innerHeight + 2;//if communication is poor with api increase '2'
             const bodyScroll = document.body.offsetHeight;
-            if(scrollY >= bodyScroll && currDate.getDate() >= endDate.getDate()){
-                currDate.setDate(currDate.getDate()-batchSize)
-                fetch(getUrl(currDate)).then(response =>
+            if(scrollY >= bodyScroll && endDate >= defaultStartDate){
+                endDate.setDate(endDate.getDate()-batchSize)
+                fetch(getUrl(endDate)).then(response =>
                     response.json()).then(response=>displayImagesBatch(response))
             }
         })
@@ -40,8 +42,8 @@ function displayImagesFromURL(event) {
 
 const getUrl = (currDate) => {
     let nasaApiUrl = "https://api.nasa.gov/planetary/apod?api_key=gqRjbR1ocVYMPviB9JoPqsVnLihxTOBKZLMGDdEm"
-    let temp = new Date()
-    temp.setDate(currDate.getDate()-3)
+    let temp = new Date(currDate)
+    temp.setDate(temp.getDate()-2)
     return `${nasaApiUrl}&start_date=${toNasaFormat(temp)}&end_date=${toNasaFormat(currDate)}`//&end_date=${startDate + 3}
 }
 
@@ -57,31 +59,48 @@ const toNasaFormat = (date) => {
 //Receives a data which is a json from nasa API that contains nasa api. displays a single batch
 function displayImagesBatch(data) {
     let currBatch = document.createElement('div')
+    let listItem
     for(let i=0; i<batchSize; i++) {
-        let listItem = initListItem()
+        if(displayIndex%2===0)
+            listItem = initListItem('row bg-success p-2 bg-opacity-25 my-3')
+        else
+            listItem = initListItem('row bg-danger bg-opacity-25 my-3')
         listItem.row.append(getImageCol(data[i]))
         listItem.row.append(getImageInfo(data[i]))
+        listItem.row.append(getMessagesCol(data[i]['date']))
         listItem.item.append(listItem.row)
         currBatch.prepend(listItem.row)
+        displayIndex ++
     }
+
     document.getElementById("imagesList").append(currBatch)
 }
-const initListItem = () => {
+const initListItem = (cname="row") => {
     let item = createElement('li','list-group-item')
-    let row = createElement('div','row')
+    let row = createElement('div',cname)
     return {item, row}
 }
 
 const getImageCol = (elem) => {
-    let imageCol = createElement('div','col-xl-8 col-md-6')
-    let img = createElement('img','img-fluid')
+    let imageCol = createElement('div','col-lg-3 col-md-6 col-sm-12')
+    let imgRow = createElement('div','row')
+    let img = createElement('img','img-thumbnail')
+    img.setAttribute('data-image',elem['url'])
     img.src = `${elem['url']}`
-    imageCol.append(img)
+    img.style.maxHeight = "400px";
+    img.style.maxWidth = "400px";
+    img.addEventListener('click', function(){
+        document.getElementById('modalImage').src = img.src
+        document.getElementById('modalImage').style.cursor = "default"
+        document.getElementById("modalBtn").click()
+    })
+    imgRow.append(img)
+    imageCol.append(imgRow)
+
     return imageCol
 }
 const getImageInfo = (elem) => {
-    let col = document.createElement('div')
-    col.className="col-xl-4 col-md-6"
+    let col = createElement('div','col-lg-5 col-md-6 col-sm-12')
     let descriptionRow = getDescriptionRow(elem)
 
     col.append(descriptionRow)
@@ -90,42 +109,99 @@ const getImageInfo = (elem) => {
 }
 
 const getDescriptionRow = (elem) => {
-
+//('row bg-danger bg-opacity-25 my-3')
     let paragraphs = getDescription(elem)
-    let descCol= appendMultiple('col',paragraphs.date,paragraphs.header,paragraphs.explanation,paragraphs.copyright)
-    let addMessageBtn = createElement('button','btn btn-secondary','Add message')
-
-    addMessageBtn.addEventListener('click', function(event){
-
-    })
-    let messageBox = getTextArea(5,33,false,"Insert message here (up to 256 characters)","50","50")
-    messageBox.setAttribute('maxlength','256')
-
-    let hideDescriptionBtn = createElement('button', 'btn btn-info',"Show less")
+    let descCol= appendMultiple('row bg-dark text-white bg-opacity-50 my-3 ',paragraphs.date,paragraphs.header,paragraphs.explanation)
+    //let descCol = appendMultiple('row',paragraphs.explanation)
+    let hideDescriptionBtn = createElement('button', 'btn btn-info bg-opacity-50 ',"Show more")
     hideDescriptionBtn.style.marginBottom="30px"
     hideDescriptionBtn.addEventListener('click', function (event) {
         event.preventDefault()
-        changeDisplay(hideDescriptionBtn,paragraphs.explanation,paragraphs.copyright, paragraphs.date)
+        changeDisplay(hideDescriptionBtn,paragraphs.explanation)
+    })
+    descCol.append(hideDescriptionBtn,paragraphs.copyright)
+
+    return descCol
+}
+const getMessagesCol = (id) => {
+    let messagesCol = createElement('div','col-lg-4 col-md-12')
+    //let firstRow = createElement('div','row') THIS WILL BE USED I'M JUST REMOVING WARNINGS
+    let secondRow = createElement('div','row')
+    let index = 0
+    let messageBox = getTextArea(5,33,false,"What's on your mind? (up to 256 characters)")
+    secondRow.append(messageBox)
+    let addMessageBtn = createElement('button','btn btn-secondary','Add message')
+    addMessageBtn.addEventListener('click', function(event){
     })
 
-    let descRow = appendMultiple("row",descCol,hideDescriptionBtn,messageBox,addMessageBtn)
-    descRow.className = "row"
+    secondRow.append(addMessageBtn)
 
-    return descRow
+    let toDisplay = (displayComments((loadComments('12',index)),id))
+    if(toDisplay !== -1)
+        messagesCol.append(toDisplay)
+    let loadMoreBtn = createElement('button',`btn btn-primary ${id}`,'Show more comments')
+    loadMoreBtn.addEventListener('click',function(){
+        index += batchSize
+        let toDisplay = displayComments(loadComments('12', index),id)
+        if(toDisplay !== -1)
+            messagesCol.append(toDisplay)
+    })
+
+    messagesCol.append(secondRow)
+    messagesCol.append(loadMoreBtn)
+    return messagesCol
+}
+let displayComments = (comments,id) =>{
+
+    let messagesList = document.getElementById(id)
+    if(messagesList === null || messagesList === undefined)
+    {
+        console.log(id)
+        messagesList  = createElement('div','list-group overflow-auto')
+        messagesList.id = id
+        messagesList.style.maxHeight = "200px"
+        messagesList.style.height = "200px"
+        messagesList.style.backgroundColor = "#7f8c8d";
+        messagesList.append(comments)
+        return messagesList
+    }
+    messagesList.append(comments)
+    return -1 //flag
 }
 
-const getTextArea = (rowsLength, colsLength, readOnly,placeHolder = "",size="",length="")=>{
-    let textBox = createElement('textarea')
-    textBox.setAttribute('rows',rowsLength)
-    textBox.setAttribute('cols',colsLength)
+function loadComments(imgDate, commentIndex) {
+    let comments = createElement('div')
+
+  fetch(`/index/messages/${imgDate}/${commentIndex}`)
+        .then(response => response.json())
+        .then(message => {
+            for(let i=0; i<batchSize; i++){
+                let listItem = initListItem('row')
+                listItem.row.innerHTML = message[i]
+                listItem.item.style.backgroundColor = "#3498db"
+                listItem.item.append(listItem.row)
+                if(listItem.row.innerHTML !== 'undefined')
+                    comments.append(listItem.item)
+            }
+            return comments
+        })
+        .catch(error => {
+            console.log(commentIndex);
+            console.error(error)
+
+        });
+
+    return comments
+}
+
+const getTextArea = (rowsLength, colsLength, readOnly,placeHolder = "")=>{
+    let textBox = createElement('textarea','form-control')
+    let div = createElement("div","form-group")
+    div.append(textBox)
+    textBox.placeholder = placeHolder
     textBox.readOnly = readOnly
-    textBox.setAttribute('size',size)
-    textBox.setAttribute('length',length)
-    textBox.style.resize = 'none';
-    textBox.placeholder =  placeHolder
-    return textBox
+    return div
 }
-
 
 const changeDisplay = (button,...paragraphs)=>{
     paragraphs.forEach(paragraph => {
@@ -159,6 +235,7 @@ const getDescription = (elem)=>{
     paragraphs.date.innerHTML = `Date: ${elem['date']}`
     paragraphs.header.innerHTML = `${elem['title']}`
     paragraphs.explanation.innerHTML = `${elem['explanation']}`
+    paragraphs.explanation.setAttribute('hidden','hidden')
     paragraphs.explanation.style.maxHeight = "300px"
     paragraphs.explanation.style.overflowY = "scroll"
     paragraphs.copyright.innerHTML =  elem['copyright'] !== undefined ? `Copyright: ${elem['copyright']}` : ""
--- routes/index.js
+++ routes/index.js
@@ -2,21 +2,54 @@ var express = require('express');
 var router = express.Router();
 
 const messagesMap = new Map()
-
+const msgArray = ['hello','world','im','lookin','4','solutions','to','the','global','warming','problem']
+messagesMap.set('12',msgArray)
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  //here (I think) we need to develop routes of the api
+
+router.get('/', function(req, res) { //I removed the parameter called next (next func)
+
   res.render('index', { title: 'Express' }); //file is index html ('index')
 });
 
 
+router.get('/index/messages/:id/:index', (req, res) => {
+  // Get the message ID from the request parameters
+  const id = req.params.id;
+  const index = req.params.index
+   //Find the message with the specified ID
+  const messages = messagesMap.get(id)
+  // If the message was not found, return a 404 status code
+  if (messages===null) { //index+3 > messages.length isnt logical
+    console.log( `index is: ${index}`)
+    res.status(450).send('Message not found' + `index is: ${index}`);
+    return;
+  }
+  let toReturn = []
+  for(let i=index; (i<index+3&& i<messages.length); i++)
+    if( messages[i] !== undefined )
+      toReturn.push( messages[i])
+
+  console.log(toReturn)
+  res.json(toReturn);
+});
+
+
+router.post('/index/messages', (req, res) => {
+  // Get the message text from the request body
+  const text = req.body.text;
+  const id = req.body.id
+  insertMessage(id,text)
+
+  // Return a success response
+  res.send('Message added successfully');
+});
 
 const insertMessage = (entry, message) => {
-  let messages_array = messagesMap.entry
-  if(messages_array === undefined)
-    messagesMap.entry = [message]
+  const messages = messagesMap.get(entry)
+  if(!messages)
+    messagesMap.set(entry, [message])
   else
-    messages_array.push(message)
+    messages.push(message)
 }
 
 
